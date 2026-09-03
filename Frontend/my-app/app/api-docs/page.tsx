93744aad0b00c6307efef27760c0ca4ff82a4de4
legendadesue-cloud <legendadesue@gmail.com>
2026-09-03 03:38:27 -0700

    Refactor useEffect and script loading logic

--- Frontend/my-app/app/api-docs/page.tsx
+++ Frontend/my-app/app/api-docs/page.tsx
@@ -1,36 +1,37 @@
 "use client";
 
-import { useEffect,useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function ApiDocs() {
   const redocContainerRef = useRef<HTMLDivElement>(null);
 
- useEffect(() => {
-  let script: HTMLScriptElement | null = null;
-  let mounted = true;
+  useEffect(() => {
+    let script: HTMLScriptElement | null = null;
 
-  if (mounted && redocContainerRef.current) {
-    script = document.createElement("script");
-    script.src = "https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js";
-    script.async = true;
+    if (redocContainerRef.current) {
+      script = document.createElement("script");
+      script.src =
+        "https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js";
+      script.async = true;
 
-    script.onload = () => {
-      // @ts-ignore
-      window.Redoc.init(
-        "http://localhost:4000/swagger.json",
-        {},
-        redocContainerRef.current
-      );
-    };
+      script.onload = () => {
+        // @ts-ignore
+        window.Redoc.init(
+          "http://localhost:4000/swagger.json",
+          {},
+          redocContainerRef.current
+        );
+      };
 
-    document.body.appendChild(script);
-  }
-
-  return () => {
-    mounted = false;
-
-    if (script) {
-      document.body.removeChild(script);
+      document.body.appendChild(script);
     }
-  };
-}, []);
+
+    return () => {
+      if (script && script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
+    };
+  }, []);
+
+  return <div ref={redocContainerRef} />;
+}
